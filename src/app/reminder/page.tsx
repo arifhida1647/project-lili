7e633bb1e8d811fd1f9add947cef7f95945d9f13
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-09 22:12:44 +0700

    update

--- src/app/reminder/page.tsx
+++ src/app/reminder/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import { addDays } from 'date-fns';
 
 const DatePicker = dynamic(() => import('react-datepicker'), { ssr: false });
 import 'react-datepicker/dist/react-datepicker.css';
@@ -10,10 +11,17 @@ const ReminderPage: React.FC = () => {
   const [reminders, setReminders] = useState<{ id: number, reminder: string, time: string }[]>([]);
   const [newReminder, setNewReminder] = useState<string>('');
   const [scheduledDate, setScheduledDate] = useState<Date | null>(null);
+  const [startDate, setStartDate] = useState<Date | null>(() => {
+    const storedStartDate = localStorage.getItem('startDate');
+    return storedStartDate ? new Date(storedStartDate) : null;
+  });
+  const [endDate, setEndDate] = useState<Date | null>(() => {
+    const storedEndDate = localStorage.getItem('endDate');
+    return storedEndDate ? new Date(storedEndDate) : null;
+  });
   const [idCounter, setIdCounter] = useState<number>(0);
   const [deleteInput, setDeleteInput] = useState<string>('');
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); // State to track modal open/close
-
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     if ('Notification' in window && Notification.permission !== 'granted') {
@@ -27,6 +35,19 @@ const ReminderPage: React.FC = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (startDate) {
+      localStorage.setItem('startDate', startDate.toISOString());
+    }
+  }, [startDate]);
+
+  useEffect(() => {
+    if (endDate) {
+      localStorage.setItem('endDate', endDate.toISOString());
+    }
+  }, [endDate]);
+
+
   const addReminder = () => {
     if (newReminder.trim() !== '' && scheduledDate) {
       const newReminderObj = { id: idCounter, reminder: newReminder, time: scheduledDate.toLocaleTimeString() };
@@ -35,7 +56,25 @@ const ReminderPage: React.FC = () => {
       localStorage.setItem('reminders', JSON.stringify(updatedReminders));
       setNewReminder('');
       setScheduledDate(null);
-      setIdCounter(prevId => prevId + 1); // Increment idCounter after adding a reminder
+      setIdCounter(prevId => prevId + 1);
+
+      // Jika tanggal mulai dan akhir diatur, tambahkan reminder untuk setiap hari di antara tanggal tersebut
+      if (startDate && endDate) {
+        let currentDate = new Date(startDate);
+        while (currentDate <= endDate) {
+          // Buat reminder untuk setiap hari pada jam yang sama
+          const reminderForDay = {
+            id: idCounter + 1,
+            reminder: newReminder,
+            time: scheduledDate.toLocaleTimeString(),
+          };
+          updatedReminders.push(reminderForDay);
+          // Increment idCounter untuk setiap reminder baru
+          setIdCounter(prevId => prevId + 1);
+          // Tambahkan satu hari ke tanggal saat ini
+          currentDate = addDays(currentDate, 1);
+        }
+      }
     }
   };
 
@@ -93,60 +132,63 @@ const ReminderPage: React.FC = () => {
         </div>
       </div>
       <div className='my-5'>
-        {reminders.map(({ id, reminder, time }) => (
+        {reminders.filter((reminder, index, self) => self.findIndex(r => r.reminder === reminder.reminder) === index).map(({ id, reminder, time }) => (
           <div key={id}>
             <div className="max-w-sm p-6 bg-green-500 border border-gray-200 rounded-lg shadow-xl mx-5 my-5" >
               <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{reminder}</h5>
+                <h5 className="mb-5 text-3xl font-bold font-serif tracking-tight text-white">{reminder}</h5>
               </a>
-              <p className="mb-3 font-normal text-white">{time}</p>
+              <div className='flex my-2'>
+                <img className='h-7 w-7 me-3' src="icontime.svg" alt="" />
+                <p className="mb-3 font-normal text-white">{time}</p>
+              </div>
+              <div className='flex '>
+                <img className='h-7 w-7 me-3' src="iconschedule.svg" alt="" />
+                <p className="mb-3 font-normal text-white">{startDate ? startDate.toLocaleDateString() : ''} - {endDate ? endDate.toLocaleDateString() : ''} </p>
+              </div>
             </div>
           </div>
         ))}
       </div>
 
       {/* <!-- Main modal --> */}
-      <div id="default-modal" tabIndex={-1} aria-hidden="true" className={`${isModalOpen ? '' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
-        <div className="relative p-4 w-full max-w-2xl max-h-full">
-          {/* <!-- Modal content --> */}
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            {/* <!-- Modal header --> */}
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Add Reminder
-              </h3>
-              <button onClick={toggleCloseModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            {/* <!-- Modal body --> */}
-            <div className="p-4 md:p-5 space-y-4">
-              <div className="my-5 mx-2">
-                <div className='text-gray-200 mb-2 text-sm'>Reminder text </div>
-                <input type="text"
-                  value={newReminder}
-                  onChange={(e) => setNewReminder(e.target.value)}
-                  placeholder="Enter reminder text" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
-              </div>
-              <div className='text-gray-200 mb-2 mt-5 mx-2 text-sm'>Date & Time</div>
-              <DatePicker
-                selected={scheduledDate}
-                onChange={(date: Date) => setScheduledDate(date)}
-                showTimeSelect
-                timeFormat="HH:mm"
-                timeIntervals={5}
-                dateFormat="MMMM d, yyyy h:mm aa"
-                placeholderText="date & time"
-                className="border-gray-300 border rounded-xl px-2 py-2 mb-2 mx-2"
-              />
-            </div>
-            {/* <!-- Modal footer --> */}
-            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-              <button className='my-5 py-3 px-5 mx-2 bg-green-500 rounded-2xl text-white' onClick={addReminder}>Add</button>
-            </div>
+      <div id="default-modal" tabIndex={-1} aria-hidden="true" className={`${isModalOpen ? '' : 'hidden'} fixed inset-0 flex items-center justify-center bg-black bg-opacity-50`}>
+        <div className="bg-white p-8 rounded-lg w-96">
+          <h3 className="text-xl font-semibold text-gray-900">Add Reminder</h3>
+          <div className="my-5 mx-2">
+            <div className='text-gray-200 mb-2 text-sm'>Reminder text</div>
+            <input type="text" value={newReminder} onChange={(e) => setNewReminder(e.target.value)} placeholder="Enter reminder text" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+          </div>
+          <div className='text-gray-200 mb-2 mt-5 mx-2 text-sm'>Date & Time</div>
+          <DatePicker
+            selected={scheduledDate}
+            onChange={(date: Date) => setScheduledDate(date)}
+            showTimeSelect
+            showTimeSelectOnly // Menampilkan hanya bagian waktu
+            timeFormat="HH:mm"
+            dateFormat="h:mm aa" // Mengatur format waktu
+            placeholderText="Select time"
+            className="border-gray-300 border rounded-xl px-2 py-2 mb-2 mx-2"
+          />
+          <div className='text-gray-200 mb-2 mt-5 mx-2 text-sm'>Start Date</div>
+          <DatePicker
+            selected={startDate}
+            onChange={(date: Date) => setStartDate(date)}
+            dateFormat="MMMM d, yyyy"
+            placeholderText="Start date"
+            className="border-gray-300 border rounded-xl px-2 py-2 mb-2 mx-2"
+          />
+          <div className='text-gray-200 mb-2 mt-5 mx-2 text-sm'>End Date</div>
+          <DatePicker
+            selected={endDate}
+            onChange={(date: Date) => setEndDate(date)}
+            dateFormat="MMMM d, yyyy"
+            placeholderText="End date"
+            className="border-gray-300 border rounded-xl px-2 py-2 mb-2 mx-2"
+          />
+          <div className="flex justify-end">
+            <button onClick={toggleModal} className="my-5 py-3 px-5 mx-2 bg-blue-500 rounded-2xl text-white">Close</button>
+            <button onClick={addReminder} className='my-5 py-3 px-5 mx-2 bg-green-500 rounded-2xl text-white'>Add</button>
           </div>
         </div>
       </div>
