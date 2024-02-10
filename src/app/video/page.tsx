5f968d6707dc2442a88c5fa472d6f4c331b50806
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-10 10:41:36 +0700

    update

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// Define type for article data
+interface ArticleData {
+    id: string;
+    title: string;
+    link: string;
+    // Add other properties as needed
+}
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAqLXB5H-9UGM7-Oj6vnd7vfF4d6jS5BYM",
+    authDomain: "coba-e6c01.firebaseapp.com",
+    projectId: "coba-e6c01",
+    storageBucket: "coba-e6c01.appspot.com",
+    messagingSenderId: "742858094690",
+    appId: "1:742858094690:web:d023c9332598049ecb793b",
+    measurementId: "G-Q4NQQF30SD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 
 export default function Video() {
+    const [articles, setArticles] = useState<ArticleData[]>([]);
+
+    useEffect(() => {
+        const fetchArticles = async () => {
+            try {
+                const articlesCollection = collection(db, 'video');
+                const snapshot = await getDocs(articlesCollection);
+                const articlesData: ArticleData[] = snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    title: doc.data().tittle,
+                    link: doc.data().link.replace(/width="\d+"/, `width="${'100%'}"`).replace(/height="\d+"/, `height="${'200'}"`)
+                }));
+                setArticles(articlesData);
+            } catch (error) {
+                console.error('Error fetching articles:', error);
+            }
+        };
+
+        fetchArticles();
+    }, []);
     return (
         <>
             <div className='flex justify-between px-5 py-5'>
@@ -13,49 +60,22 @@ export default function Video() {
                 <div className='font-bold text-3xl text-green-500 pt-1'>Video</div>
                 <div></div>
             </div>
-            <div>
-                <Link href="#" passHref>
-                    <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-xl mx-6 mb-5">
-                        <div>
-                            <a className="mt-3" href="https://www.youtube.com/watch?v=v9XyIGXcRck" target="_blank">
-                                <iframe width="100%" height="200" src="https://www.youtube.com/embed/M5QY2_8704o" title="Chillstep Music for Programming / Cyber / Coding" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-                            </a>
-                            <div className="flex flex-col justify-between p-4 leading-normal">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+            {articles.map(article => (
+                <div key={article.id}>
+                    <Link href="#" passHref>
+                        <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-xl mx-6 mb-5">
+                            <div>
+                                <a className="mt-3" href="https://www.youtube.com/watch?v=v9XyIGXcRck" target="_blank">
+                                   <div dangerouslySetInnerHTML={{ __html: article.link }}></div>
+                                </a>
+                                <div className="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">{article.title}</h5>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </Link>
-            </div>
-            <div>
-                <Link href="#" passHref>
-                    <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-xl mx-6 mb-5">
-                        <div>
-                            <a className="mt-3" href="https://www.youtube.com/watch?v=v9XyIGXcRck" target="_blank">
-                                <iframe width="100%" height="200" src="https://www.youtube.com/embed/M5QY2_8704o" title="Chillstep Music for Programming / Cyber / Coding" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-                            </a>
-                            <div className="flex flex-col justify-between p-4 leading-normal">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div>
-                <Link href="#" passHref>
-                    <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-xl mx-6 mb-5">
-                        <div>
-                            <a className="mt-3" href="https://www.youtube.com/watch?v=v9XyIGXcRck" target="_blank">
-                                <iframe width="100%" height="200" src="https://www.youtube.com/embed/M5QY2_8704o" title="Chillstep Music for Programming / Cyber / Coding" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-                            </a>
-                            <div className="flex flex-col justify-between p-4 leading-normal">
-                                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            
+                    </Link>
+                </div>
+            ))}
         </>
     );
 }
