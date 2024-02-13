7b067d416527b34b69094915cf2c741c5870a8f8
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-13 07:15:03 +0700

    update reminder

--- src/app/reminder/page.tsx
+++ src/app/reminder/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
+import Link from 'next/link';
 
 const DB_NAME = 'reminderDB';
 const STORE_NAME = 'reminders';
@@ -43,6 +43,7 @@ const ReminderApp: React.FC = () => {
     time: '',
   });
   const [repeatDays, setRepeatDays] = useState<number>(0);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     openDatabase()
@@ -58,14 +59,14 @@ const ReminderApp: React.FC = () => {
 
   useEffect(() => {
     if (db) {
-      reminders.forEach(reminder => {
+      reminders.forEach((reminder) => {
         sendNotification(reminder.title, reminder.date, reminder.time);
       });
     }
   }, [reminders]);
 
   const requestNotificationPermission = () => {
-    Notification.requestPermission().then(permission => {
+    Notification.requestPermission().then((permission) => {
       if (permission !== 'granted') {
         console.error('Permission denied for notifications');
       }
@@ -133,6 +134,7 @@ const ReminderApp: React.FC = () => {
     // Reset state for new input
     setNewReminder({ id: '', title: '', date: '', time: '' });
     setRepeatDays(0);
+    setIsModalOpen(false); // Tutup modal setelah menambahkan pengingat
   };
 
   const deleteReminder = (id: string) => {
@@ -171,50 +173,125 @@ const ReminderApp: React.FC = () => {
 
   return (
     <div>
-      <Head>
-        <title>Reminder App</title>
-        <meta name="description" content="Your reminder app description" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1>Reminder App</h1>
+      <main className='mx-2'>
+        <div className='flex justify-between py-5'>
+          <div className='p-2 bg-green-500 rounded-xl '>
+            <Link href="/">
+              <img src="iconprev.svg" />
+            </Link>
+          </div>
+          <div className='font-bold text-3xl text-green-500 pt-1'>Reminder</div>
+          <div></div>
+        </div>
         <div>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4 flex justify-center"
+          >
+            Add Reminder
+          </button>
+        </div>
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          newReminder={newReminder}
+          setNewReminder={setNewReminder}
+          repeatDays={repeatDays}
+          setRepeatDays={setRepeatDays}
+          addReminder={addReminder}
+        />
+        <ol className="relative border-s mt-10 mx-5 border-gray-200">
+          {reminders.map((reminder) => (
+            <li className="mb-10 ms-6 " key={reminder.id}>
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white ">
+                <svg className="w-2.5 h-2.5 text-blue-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                </svg>
+              </span>
+              <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900">{reminder.title}</h3>
+              <time className="block mb-2 text-sm font-normal leading-none text-gray-400">{reminder.date} - {reminder.time}</time>
+              <button onClick={() => deleteReminder(reminder.id)} className='inline-flex items-center px-4 py-2 text-sm font-medium text-gray-100 bg-red-600 border border-gray-200 rounded-lg hover:bg-red-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700'>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ol>
+      </main>
+    </div>
+  );
+};
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  newReminder: Reminder;
+  setNewReminder: React.Dispatch<React.SetStateAction<Reminder>>;
+  repeatDays: number;
+  setRepeatDays: React.Dispatch<React.SetStateAction<number>>;
+  addReminder: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  newReminder,
+  setNewReminder,
+  repeatDays,
+  setRepeatDays,
+  addReminder,
+}) => {
+  return (
+    <div className={`fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 ${isOpen ? '' : 'hidden'}`}>
+      <div className="bg-white rounded-lg p-8 max-w-sm w-full">
+        <h2 className="text-xl font-bold mb-4">Add Reminder</h2>
+        <div className="space-y-4">
+          <div className='text-gray-600 mt-2'>Tittle Reminder</div>
           <input
             type="text"
             placeholder="Enter reminder title"
             value={newReminder.title}
             onChange={(e) => setNewReminder({ ...newReminder, title: e.target.value })}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
           />
+          <div className='text-gray-600 mt-2'>Date</div>
           <input
             type="date"
             value={newReminder.date}
             onChange={(e) => setNewReminder({ ...newReminder, date: e.target.value })}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
           />
+          <div className='text-gray-600 mt-2'>Time</div>
           <input
             type="time"
             value={newReminder.time}
             onChange={(e) => setNewReminder({ ...newReminder, time: e.target.value })}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
           />
+          <div className='text-gray-600 mt-2'>Repeat Day</div>
           <input
             type="number"
             placeholder="Repeat days"
             value={repeatDays}
             onChange={(e) => setRepeatDays(parseInt(e.target.value))}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
           />
-          <button onClick={addReminder}>Add Reminder</button>
+          <div className="flex justify-end">
+            <button
+              onClick={onClose}
+              className="bg-blue-500 mx-2 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={addReminder}
+              className="bg-green-400 text-white px-4 py-2 rounded hover:bg-green-500"
+            >
+              Add Reminder
+            </button>
+          </div>
         </div>
-
-        <ul>
-          {reminders.map((reminder) => (
-            <li key={reminder.id}>
-              <div>{reminder.title}</div>
-              <div>{reminder.date} - {reminder.time}</div>
-              <button onClick={() => deleteReminder(reminder.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </main>
+      </div>
     </div>
   );
 };
