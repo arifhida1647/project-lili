aff24aef9ec33ef283b15ed01b6c38c1bf175b7b
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-17 11:28:12 +0700

    update reminder

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -49,7 +49,7 @@ const IndexPage = () => {
   const handleTestClick = async () => {
     // Memeriksa apakah semua input terisi
     if (!cronHour || !cronMinute || !fromEmail || !fromSubject) {
-      alert('Harap isi semua input!');
+      alert('Harap isi semua!');
       return;
     }
     const cronHourInt = parseInt(cronHour);
@@ -180,15 +180,18 @@ const IndexPage = () => {
             <path fill-rule="evenodd" d="M12.5 8.7a2.5 2.5 0 0 1 3.5 0 2.5 2.5 0 0 1 0 3.5l-1.1 1a1 1 0 0 0-.2-.2l-3-3-.3-.2 1.1-1Zm-2.4 2.5L7.3 14a1 1 0 0 0-.3.7v2c0 .6.4 1 1 1h2c.3 0 .5 0 .7-.3l2.8-2.8-.2-.2-3-3-.2-.2Z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M7 3c.6 0 1 .4 1 1v1h3V4a1 1 0 1 1 2 0v1h3V4a1 1 0 1 1 2 0v1h1a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h1V4c0-.6.4-1 1-1Zm10.7 8H19v8H5v-8h3.9l.5-.5c.2-.3.5-.4.9-.3 0 0 .2.1.2 0V10a1 1 0 0 1 .2-.9l1.1-1a3.5 3.5 0 0 1 4.9 0 3.5 3.5 0 0 1 1 2.9Z" clip-rule="evenodd" />
           </svg>
-          <p className="mt-1text-gray-700 text-sm font-bold mb-2">Daftar Reminder:</p>
+          <p className="mt-1text-gray-700 text-sm font-bold mb-3">Daftar Reminder:</p>
           </div>
-          <ul>
+          <ol>
             {cronJobs.map((cronJob) => (
-              <li key={cronJob.cron_job_id} className="text-gray-700 text-sm flex">
-                {cronJob.cron_job_id} - {cronJob.fromSubject} - {cronJob.cronHour.padStart(2, '0')}.{cronJob.cronMinute.padStart(2, '0')}
+              <li key={cronJob.cron_job_id} className="text-gray-700 text-sm flex mb-3">
+                ID: {cronJob.cron_job_id} <br /> 
+                Nama Obat: {cronJob.fromSubject} <br /> 
+                Jam: {cronJob.cronHour.padStart(2, '0')}.{cronJob.cronMinute.padStart(2, '0')}
+                <hr className="h-px my-8 bg-gray-600 border-4"></hr>
               </li>
             ))}
-          </ul>
+          </ol>
         </div>
       </div>
     </div>
