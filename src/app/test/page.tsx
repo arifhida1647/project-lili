f891a9af2f31c04155ed13c8ec639485919645b6
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-17 11:32:52 +0700

    update reminder

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react';
 import { openDB, DBSchema } from 'idb';
+import Link from 'next/link';
 
 interface CronJob {
   cron_job_id: string;
@@ -134,7 +135,17 @@ const IndexPage = () => {
   };
 
   return (
+
     <div className="max-w-md mx-auto mt-8">
+      <div className='flex justify-between px-5 py-5'>
+        <div className='p-2 bg-green-500 rounded-xl '>
+          <Link href="/">
+            <img src="iconprev.svg" />
+          </Link>
+        </div>
+        <div className='font-bold text-3xl text-green-500 pt-1'>Remind By Email</div>
+        <div></div>
+      </div>
       <div className="bg-white shadow-md rounded px-8 py-8">
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">Jam (0-23):</label>
@@ -176,17 +187,17 @@ const IndexPage = () => {
         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleDeleteJob}>Delete</button>
         <div className="mt-9">
           <div className='flex'>
-          <svg className="w-6 h-6 text-gray-800 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M12.5 8.7a2.5 2.5 0 0 1 3.5 0 2.5 2.5 0 0 1 0 3.5l-1.1 1a1 1 0 0 0-.2-.2l-3-3-.3-.2 1.1-1Zm-2.4 2.5L7.3 14a1 1 0 0 0-.3.7v2c0 .6.4 1 1 1h2c.3 0 .5 0 .7-.3l2.8-2.8-.2-.2-3-3-.2-.2Z" clip-rule="evenodd" />
-            <path fill-rule="evenodd" d="M7 3c.6 0 1 .4 1 1v1h3V4a1 1 0 1 1 2 0v1h3V4a1 1 0 1 1 2 0v1h1a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h1V4c0-.6.4-1 1-1Zm10.7 8H19v8H5v-8h3.9l.5-.5c.2-.3.5-.4.9-.3 0 0 .2.1.2 0V10a1 1 0 0 1 .2-.9l1.1-1a3.5 3.5 0 0 1 4.9 0 3.5 3.5 0 0 1 1 2.9Z" clip-rule="evenodd" />
-          </svg>
-          <p className="mt-1text-gray-700 text-sm font-bold mb-3">Daftar Reminder:</p>
+            <svg className="w-6 h-6 text-gray-800 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd" d="M12.5 8.7a2.5 2.5 0 0 1 3.5 0 2.5 2.5 0 0 1 0 3.5l-1.1 1a1 1 0 0 0-.2-.2l-3-3-.3-.2 1.1-1Zm-2.4 2.5L7.3 14a1 1 0 0 0-.3.7v2c0 .6.4 1 1 1h2c.3 0 .5 0 .7-.3l2.8-2.8-.2-.2-3-3-.2-.2Z" clip-rule="evenodd" />
+              <path fill-rule="evenodd" d="M7 3c.6 0 1 .4 1 1v1h3V4a1 1 0 1 1 2 0v1h3V4a1 1 0 1 1 2 0v1h1a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h1V4c0-.6.4-1 1-1Zm10.7 8H19v8H5v-8h3.9l.5-.5c.2-.3.5-.4.9-.3 0 0 .2.1.2 0V10a1 1 0 0 1 .2-.9l1.1-1a3.5 3.5 0 0 1 4.9 0 3.5 3.5 0 0 1 1 2.9Z" clip-rule="evenodd" />
+            </svg>
+            <p className="mt-1text-gray-700 text-sm font-bold mb-3">Daftar Reminder:</p>
           </div>
           <ol>
             {cronJobs.map((cronJob) => (
               <li key={cronJob.cron_job_id} className="text-gray-700 text-sm flex mb-3">
-                ID: {cronJob.cron_job_id} <br /> 
-                Nama Obat: {cronJob.fromSubject} <br /> 
+                ID: {cronJob.cron_job_id} <br />
+                Nama Obat: {cronJob.fromSubject} <br />
                 Jam: {cronJob.cronHour.padStart(2, '0')}.{cronJob.cronMinute.padStart(2, '0')}
                 <hr className="h-px my-8 bg-gray-600 border-4"></hr>
               </li>
