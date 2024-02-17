6aa3538f990afefaf488298c8694e158e07d8e52
arif hidayat <90981017+arifhida1647@users.noreply.github.com>
2024-02-17 11:08:46 +0700

    update reminder

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,10 +5,12 @@ import { openDB, DBSchema } from 'idb';
 interface CronJob {
   cron_job_id: string;
   fromSubject: string;
+  cronHour: string;
+  cronMinute: string;
 }
 
 const IndexPage = () => {
-  const [cronHour, setCronHour] = useState<string>('*');
+  const [cronHour, setCronHour] = useState<string>('');
   const [cronMinute, setCronMinute] = useState<string>('0');
   const [fromEmail, setEmail] = useState<string>('');
   const [fromSubject, setSubject] = useState<string>('');
@@ -45,15 +47,36 @@ const IndexPage = () => {
   };
 
   const handleTestClick = async () => {
+    // Memeriksa apakah semua input terisi
+    if (!cronHour || !cronMinute || !fromEmail || !fromSubject) {
+      alert('Harap isi semua input!');
+      return;
+    }
+    const cronHourInt = parseInt(cronHour);
+    if (isNaN(cronHourInt) || cronHourInt < 0 || cronHourInt > 23) {
+      alert('Harap masukkan nilai untuk Jam dalam rentang 0-23!');
+      return;
+    }
+    const cronMinuteInt = parseInt(cronMinute);
+    if (isNaN(cronMinuteInt) || cronMinuteInt < 0 || cronMinuteInt > 59) {
+      alert('Harap masukkan nilai untuk Minute dalam rentang 0-59!');
+      return;
+    }
+
+    // Memeriksa apakah alamat email memiliki format yang benar
+    const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailPattern.test(fromEmail)) {
+      alert('Harap masukkan alamat email yang valid!');
+      return;
+    }
     setIsLoading(true);
     try {
       const cron_expression = `${cronMinute} ${cronHour} * * *`;
-
       const data = {
         token: '8715e02850fc5298cb8115e29be384cf',
         cron_expression: cron_expression,
         url: `https://temp-ten-lilac.vercel.app/home?to=${fromEmail}&subject=${fromSubject}&text=reminder`,
-        cron_job_name: 'Nama Pekerjaan Cron Anda'
+        cron_job_name: fromSubject
       };
 
       const response = await fetch('https://server-temp.vercel.app/home/create-job', {
@@ -67,7 +90,12 @@ const IndexPage = () => {
       const jsonData = await response.json();
       console.log(jsonData);
 
-      saveCronJob({ cron_job_id: jsonData.cron_job_id, fromSubject });
+      saveCronJob({
+        cron_job_id: jsonData.cron_job_id,
+        fromSubject,
+        cronHour,
+        cronMinute
+      })
       loadCronJobs();
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -130,7 +158,7 @@ const IndexPage = () => {
           <label className="block text-gray-700 text-sm font-bold mb-2">Subject:</label>
           <input required type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={fromSubject} onChange={(e) => setSubject(e.target.value)} />
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center mb-4">
           {isLoading ? (
             <div role="status">
               <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -142,14 +170,23 @@ const IndexPage = () => {
           ) : (
             <button className="bg-blue-500 me-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleTestClick}>Add</button>
           )}
-          <input type="text" placeholder="Cron Job ID" className="me-2 shadow appearance-none border rounded w-48 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={deleteCronJobId} onChange={(e) => setDeleteCronJobId(e.target.value)} />
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleDeleteJob}>Delete</button>
         </div>
-        <div className="mt-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Cron Job ID:</label>
+        <input type="text" placeholder="" className="me-2 shadow appearance-none border rounded w-48 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={deleteCronJobId} onChange={(e) => setDeleteCronJobId(e.target.value)} />
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleDeleteJob}>Delete</button>
+        <div className="mt-9">
           <p className="text-gray-700 text-sm font-bold mb-2">Daftar Cron Jobs:</p>
           <ul>
+
+
             {cronJobs.map((cronJob) => (
-              <li key={cronJob.cron_job_id} className="text-gray-700 text-sm">{cronJob.cron_job_id} - {cronJob.fromSubject}</li>
+              <li key={cronJob.cron_job_id} className="text-gray-700 text-sm flex">
+                <svg className="w-6 h-6 text-gray-800 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d="M12.5 8.7a2.5 2.5 0 0 1 3.5 0 2.5 2.5 0 0 1 0 3.5l-1.1 1a1 1 0 0 0-.2-.2l-3-3-.3-.2 1.1-1Zm-2.4 2.5L7.3 14a1 1 0 0 0-.3.7v2c0 .6.4 1 1 1h2c.3 0 .5 0 .7-.3l2.8-2.8-.2-.2-3-3-.2-.2Z" clip-rule="evenodd" />
+                  <path fill-rule="evenodd" d="M7 3c.6 0 1 .4 1 1v1h3V4a1 1 0 1 1 2 0v1h3V4a1 1 0 1 1 2 0v1h1a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h1V4c0-.6.4-1 1-1Zm10.7 8H19v8H5v-8h3.9l.5-.5c.2-.3.5-.4.9-.3 0 0 .2.1.2 0V10a1 1 0 0 1 .2-.9l1.1-1a3.5 3.5 0 0 1 4.9 0 3.5 3.5 0 0 1 1 2.9Z" clip-rule="evenodd" />
+                </svg>
+                {cronJob.cron_job_id} - {cronJob.fromSubject} - {cronJob.cronHour.padStart(2, '0')}.{cronJob.cronMinute.padStart(2, '0')}
+              </li>
             ))}
           </ul>
         </div>
